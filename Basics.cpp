16b423322efd67a4b7d45910d2b36b56c197633d
sbayat023 <sbayat023@student.wethinkcode.co.za>
2024-12-18 00:24:45 +0200

    Added input that works with stream

--- Basics.cpp
+++ Basics.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-int main(){
-    cout << "Hello World\n";
+string name;
+
+void getName(){
+    getline(cin,name);
+}
+
+void calFormula(){
+    string formula,temp,split;
+    int index = 0;
+    string formulaSplit[100];
+
+    getline(cin,formula);
+
+    istringstream stream(formula);
+
+    while (getline(stream, temp, ' ')) {
+        formulaSplit[index++] = temp;
+    }
+
+    for (int i = 0; i < index; i++) {
+        cout << formulaSplit[i] << endl;
+    }
+
+}
+
+int main()
+{
+    cout << "Enter your name : ";
+    getName();
+
+    cout << "Hello " + name + "\n";
+
+    cout << "Enter a +,-,*,/ formula.\nExample 1 + 1\nEnter your formula : ";
+    calFormula();
     return 0;
 }
