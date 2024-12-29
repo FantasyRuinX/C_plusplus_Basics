3af24f5f0871ceacbe5cb76ce21ef239c4cf692e
sbayat023 <sbayat023@student.wethinkcode.co.za>
2024-12-29 13:07:47 +0200

    Added operator checks

--- Basics.cpp
+++ Basics.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <format>
 
 using namespace std;
 
 string name,formula;
 string formulaSplit[100];
 
-void getName(){
-    getline(cin,name);
-}
 
 void subString(){
     string temp;
@@ -27,24 +25,38 @@ void calFormula(){
     string temp;
     int index = 0;
 
-    getline(cin,formula);
+    int num0 = 0,num1 = 0;
 
+    getline(cin,formula);
     subString();
 
     for (int i = 0; i <= formulaSplit->length() + 1; i++) {
-        cout << formulaSplit[i] << endl;
+        string temp = formulaSplit[i];
+        bool isNum = isdigit(temp[0]);
+
+        if (isNum && num0 == 0){
+            num0 = stoi(temp);
+        }else if (isNum && num1 == 0){
+            num1 = stoi(temp);
+        }
     }
 
+
+    if (formulaSplit[1] == "+"){ cout << num0 << " + " << num1 << " = " << (num0 + num1) << endl;}
+    else if (formulaSplit[1] == "-"){ cout << num0 << " - " << num1 << " = " << (num0 - num1) << endl;}
+    else if (formulaSplit[1] == "*"){ cout << num0 << " * " << num1 << " = " << (num0 * num1) << endl;}
+    else if (formulaSplit[1] == "/"){ cout << num0 << " / " << num1 << " = " << (num0 / num1) << endl;}
 }
 
 int main()
 {
     cout << "Enter your name : ";
-    getName();
+    getline(cin,name);
 
     cout << "Hello " + name + "\n";
 
     cout << "Enter a +,-,*,/ formula.\nExample 1 + 1\nEnter your formula : ";
     calFormula();
+
     return 0;
 }
